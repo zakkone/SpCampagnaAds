17adf3c2e7ebdd0d9668eff25626ef7ba1d4b0ad
zakkone <pierluigi.spalletta@gmail.com>
2016-02-13 11:38:29 +0100

    init SpCampagnaAds

---
+++ index.php
@@ -0,0 +1,278 @@
+<? require_once "ccookie.php"; ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+  <title>Template Landing page Gratis: www.alfonsostriano.it</title>
+  <meta name="description" content="Cambiare vita, guadagnare risparmiando, far fruttare le proprie passioni."/>
+   <!-- FB Open Graph sostituire con le proprie informazioni -->
+  <meta property="og:url" content="http://www.alfonsostriano.it" /> 
+  <meta property="og:title" content="Alfonso Striano | Web Marketing" /> 
+  <meta property="og:image" content="http://www.alfonsostriano.it/wp-content/uploads/2012/07/Alfonso-FB-1.jpg" /> 
+  <meta property="og:description" content="Cambiare vita, guadagnare risparmiando, far fruttare le proprie passioni." />
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <!-- Google Font -->
+  <link href='https://fonts.googleapis.com/css?family=Raleway|Satisfy' rel='stylesheet' type='text/css'>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <!-- SumoMe sostituire il codice seguente con il proprio -->
+  <script src="//load.sumome.com/" data-sumo-site-id="f798130d4a232c41b3c90168199dd0b99b7bacac6a114015d8f971d0142a9276" async="async"></script>
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="/favicontoolboxstriano.ico" />
+  <link rel="icon" href="/favicontoolboxstriano.ico" />
+
+</head>
+<body>
+  <!-- Go To Top Button -->
+  <a href="#" class="back-to-top" style="display: inline;">
+  <i class="fa fa-arrow-circle-up"></i>
+  </a>
+  <!-- Fixed Navbar -->
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper white">
+        <a href="#" class="brand-logo">Landing Page</a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+          <li><a href="#servizio">Servizio</a></li>
+          <li><a href="#registrazione">Registrati</a></li>
+           <li><a href="#ospita">Offerta</a></li>
+        </ul>
+         <ul class="side-nav" id="mobile-demo">
+            <li><a href="#servizio">Servizio</a></li>
+          <li><a href="#registrazione">Registrati</a></li>
+           <li><a href="#ospita">Offerta</a></li>
+      </ul>
+      </div>
+    </nav>
+  </div>
+
+<!-- Intro -->
+  <div id="index-banner" class="parallax-container">
+    <div class="section-intro no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header left-align white-text"><b>Un Templete già pronto!</b></h1>
+        <div class="row center">
+          <h2 class="header-subtitle col s12 light"><br><br><b>È molto veloce <br>ed è già responsive.</b></h2>
+        </div>
+        <div class="row center">
+          <a href="#servizio" class="btn-large waves-effect waves-light red lighten-1">Scopri come funziona</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+    <div class="parallax"><img src="images/background1.jpg" alt="Unsplashed background img 1"></div>
+  </div>
+
+<!-- Servizio -->
+  <div id="servizio" class="container">
+    <div class="section">
+      <div class="col-servizio">
+
+        <!--   Icon Section   -->
+        <div class="row">
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">tablet_android</i></h2>
+              <h5 class="center">Template Materialize</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">Modello predisposto dal team di Google</h5>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">android</i></h2>
+              <h5 class="center">Material design</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">Design molto simile a quello di android</h5>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">mood</i></h2>
+              <h5 class="center">SEO e Social Media</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">
+Il Template è già ottimizzato per SEO e Social Media</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Altri Benefici -->
+  <!-- Servizio -->
+  <div id="servizio2" class="container">
+    <div class="section">
+      <div class="col-benefici">
+
+        <!--   Icon Section   -->
+        <div class="row">
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">invert_colors</i></h2>
+              <h5 class="center">User expierence</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">Dotato di colori compatibili per l’user expierence</h5>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">group_add</i></h2>
+              <h5 class="center">Aumenta gli iscritti</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">Aumenta il numero dei tuoi iscritti, le vendite e i guadagni</h5>
+            </div>
+          </div>
+
+          <div class="col s12 m4">
+            <div class="icon-block">
+              <h2 class="center red-text text-lighten-1"><i class="material-icons">important_devices</i></h2>
+              <h5 class="center">Responsive</h5>
+              <br>
+              <h5 class="center red-text text-lighten-1 light">È molto veloce e già responsive</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-- Registrazione -->
+  <div id="registrazione" class="parallax-container valign-wrapper">
+    <div class="section-registrazione no-pad-bot">
+      <div class="container">
+        <div class="row center">
+          <i class="material-icons registrazione-icon">important_devices</i>
+          <h3 class="col s12 light"><b>Vuoi questa Landing page GRATIS? <br>Compila il form con la tua mail!</b></h3>
+          <h5 class="col s12 registrazione-subtitle"><b>Registrati per scaricare il mio template Gratis!</b></h5>
+           <div class="row">
+              <div class="input-field col s12 l6 offset-l3">
+    <!-- SendinBlue Signup Form HTML Code sostituire il codice seguente con il proprio -->
+<div id="sib_embed_signup">
+    <div class="wrapper">
+
+        <input type="hidden" id="sib_embed_signup_lang" value="it">
+        <input type="hidden" id="sib_embed_invalid_email_message" value="Email non valida, Riprova.">
+        <div id="sib_loading_gif_area" style="position: absolute;z-index: 9999;display: none;">
+          <img src="https://my.sendinblue.com/public/theme/version3/images/loader_sblue.gif" style="display: block;margin-left: auto;margin-right: auto;position: relative;top: 40%;">
+        </div>
+         
+      <form class="description" id="theform" name="theform" action="https://my.sendinblue.com/users/subscribeembed/js_id/262o2/id/8" onsubmit="return false;">
+         
+            <input type="hidden" name="js_id" id="js_id" value="262o2"><input type="hidden" name="listid" id="listid" value="10">
+            <input type="hidden" name="from_url" id="from_url" value="yes">
+            <input type="hidden" name="hdn_email_txt" id="hdn_email_txt" value="">
+           
+            <input name="req_hid" id="req_hid" value="" type="hidden">
+            
+            <input name="email" id="email" value="" type="text" class="validate white center black-text" placeholder="Inserisci qui la tua email!">
+  
+            <button class="btn btn-registrazione waves-effect waves-light red lighten-1" type="submit" data-editfield="subscribe" name="action">Voglio questa Landing page!</button>     
+
+            <p><center><img src="images/lucchetto.png" alt="privacy" style="max-width: 16px; max-height: 16px;">  100% Sicuro. Odio lo Spam quanto te.</p></center>
+
+            <div class="container rounded" style="position: absolute;top:150px;left:75px;"> 
+
+            </div>      
+        </form>
+    </div>
+</div>
+<script type='text/javascript' src='https://my.sendinblue.com/public/theme/version3/js/subscribe-validate.js'></script>
+<script type='text/javascript'>
+    jQuery.noConflict(true);
+</script>
+<!-- End : SendinBlue Signup Form HTML Code -->
+
+              </div>
+              </div>
+        </div>
+      </div>
+    </div>
+    <div class="parallax"><img src="images/background2.jpg" alt="Unsplashed background img 2"></div>
+  </div>
+
+  <!-- Offerte -->
+  <div id="ospita" class="container">
+    <div class="section">
+      <div class="row">
+        <div class="col s12 center">
+          <i class="material-icons ospita-icon red-text text-lighten-1">card_giftcard</i>
+          <h3>Sondaggio: quali argomenti preferisci?</h3>
+          <h5 class="col s12 ospita-subtitle">Compila il questionario per ricevere le mie risorse premium. 
+          <br>
+          Ti ricontatteremo entro 24h.</h5>
+          <a class="typeform-share col l6 offset-l3 btn btn-ospita waves-effect waves-light red lighten-1" href="https://alfonsostriano.typeform.com/to/OEuSrM" data-mode="2" target="_blank">Dammi il tuo Feedback!</a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+<!-- Footer -->
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col l12 s12 center">
+          <h5 class="white-text">Contattami</h5>
+          <a href="mailto:alfonso.striano@gmail.com?subject=Richiesta%20informazioni%20LandingPage" class="social-icon"><i class="fa fa-envelope"></i></a>
+           <a href="https://www.facebook.com/alfonsostriano.it/" target="_blank" class="social-icon"><i class="fa fa-facebook"></i></a>
+           <a href="https://plus.google.com/+Alfonsostrianoitok?prsrc=5" target="_blank" class="social-icon"><i class="fa fa-google-plus"></i></a>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      <h5 class="center container-footer">2015 &copy <a href="http://www.alfonsostriano.it" target="_blank">AlfonsoStriano.it</a> | <a href="http://www.alfonsostriano.it/privacy/" target="_blank">Privacy Policy</a></h5>
+      </div>
+    </div>
+  </footer>
+ 
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  <!-- TypeForm sostituire il codice seguente con il proprio -->
+  <script>
+<script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}id=id+'_';if(!gi.call(d,id)){qs=ce.call(d,'link');qs.rel='stylesheet';qs.id=id;qs.href=b+'share-button.css';s=gt.call(d,'head')[0];s.appendChild(qs,s)}})()</script>
+  <!-- Analytics sostituire il codice seguente con il proprio -->
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-54285732-1', 'auto');
+  ga('send', 'pageview');
+</script>
+<!-- Go to Top Button -->
+<script>
+jQuery(document).ready(function() {
+var offset = 200;
+var duration = 500;
+jQuery(window).scroll(function() {
+if (jQuery(this).scrollTop() > offset) {
+jQuery('.back-to-top').fadeIn(duration);
+} else {
+jQuery('.back-to-top').fadeOut(duration);
+}
+});
+jQuery('.back-to-top').click(function(event) {
+event.preventDefault();
+jQuery('html, body').animate({scrollTop: 0}, duration);
+return false;
+})
+});
+</script>
+  </body>
+</html>
+<? ccookie(); ?>
